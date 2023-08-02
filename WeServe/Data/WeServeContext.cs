113312248754f1f1902dd46fe45c38a81d23bbd0
Justin Kruskie <justin@jkruskie.com>
2023-08-02 11:54:47 -0400

    Implemented dummy data for each role

--- WeServe/Data/WeServeContext.cs
+++ WeServe/Data/WeServeContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using WeServe.Models;
 
 namespace WeServe.Data
@@ -36,6 +37,66 @@ namespace WeServe.Data
         {
             // Configure the models
             modelBuilder.Entity<User>().ToTable("Users");
+
+            // Default password
+            var password = "password123$";
+            // Hash the password
+            var passwordHasher = new PasswordHasher<User>();
+            var passwordHash = passwordHasher.HashPassword(null, password);
+
+            // Seed the database
+            modelBuilder.Entity<User>().HasData(
+                new User
+                {
+                    Id = 1,
+                    FirstName = "Justin",
+                    LastName = "Kruskie",
+                    Email = "justin@jkruskie.com",
+                    NormalizedEmail = "JUSTIN@JKRUSKIE.COM",
+                    Role = "Admin",
+                    PasswordHash = passwordHash
+                },
+                new User
+                {
+                    Id = 2,
+                    FirstName = "Student",
+                    LastName = "Account",
+                    Email = "student@weserve.com",
+                    NormalizedEmail = "STUDENT@WESERVE.COM",
+                    Role = "Student",
+                    PasswordHash = passwordHash
+                },
+                new User
+                {
+                    Id = 3,
+                    FirstName = "Organization",
+                    LastName = "Account",
+                    Email = "organization@weserve.com",
+                    NormalizedEmail = "ORGANIZATION@WESERVE.COM",
+                    Role = "Organization",
+                    PasswordHash = passwordHash
+                },
+                new User
+                {
+                    Id = 4,
+                    FirstName = "Moderator",
+                    LastName = "Account",
+                    Email = "moderator@weserve.com",
+                    NormalizedEmail = "MODERATOR@WESERVE.COM",
+                    Role = "Moderator",
+                    PasswordHash = passwordHash
+                },
+                new User
+                {
+                    Id = 5,
+                    FirstName = "Admin",
+                    LastName = "Account",
+                    Email = "admin@weserve.com",
+                    NormalizedEmail = "ADMIN@WESERVE.COM",
+                    Role = "Admin",
+                    PasswordHash = passwordHash
+                }
+            );
         }
     }
 }
